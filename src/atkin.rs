44f6afcf83a871b0dc38be6ceb611161a0acf51f
Arnau Siches <asiches@gmail.com>
2017-01-22 11:44:34 +0000

    feat(atkin): Dummy baseline test
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- src/atkin.rs
+++ src/atkin.rs
@@ -7,3 +7,16 @@ pub fn sieve(amount: usize) -> Vec<usize> {
     vec![2,3,5]
 }
 
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn three_primes() {
+        let actual = sieve(3);
+        assert_eq!(&actual, &[2, 3, 5]);
+    }
+
+}
