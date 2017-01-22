85df5fbc4494c2f1fc54a5f67e6790dd433ff910
Arnau Siches <asiches@gmail.com>
2017-01-22 11:55:50 +0000

    feat(atkin): Cover cases 0, 1, 2, 3
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- src/atkin.rs
+++ src/atkin.rs
@@ -4,7 +4,15 @@
 
 
 pub fn sieve(amount: usize) -> Vec<usize> {
-    vec![2,3,5]
+    let mut primes: Vec<usize> = vec![2, 3, 5];
+
+    match amount {
+        3 => primes,
+        2 => primes.drain(2..).collect(),
+        1 => primes.drain(1..).collect(),
+        0 => vec![],
+        _ => vec![],
+    }
 }
 
 
@@ -14,9 +22,20 @@ mod tests {
     use super::*;
 
     #[test]
+    fn one_prime() {
+        let actual = sieve(1);
+        assert_eq!(&actual, &[2]);
+    }
+
+    #[test]
+    fn one_prime() {
+        let actual = sieve(2);
+        assert_eq!(&actual, &[2, 5]);
+    }
+
+    #[test]
     fn three_primes() {
         let actual = sieve(3);
         assert_eq!(&actual, &[2, 3, 5]);
     }
-
 }
